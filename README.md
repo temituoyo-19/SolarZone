b8a59ae74d23415b6bac4c037ef2465efe4241d3
Temisanren Temituoyo <106888033+temituoyo-19@users.noreply.github.com>
2024-08-16 10:02:18 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+"# SolarZone-web-app" 
+"# SolarZone-web-app" 
+"# SolarZone" 
---
+++ testimonial.js
@@ -0,0 +1,7 @@
+const joinTheseGuys = document.getElementById('join')
+
+// Add a click event listener to the button
+joinTheseGuys.addEventListener('click', function() {
+
+  alert('This feauture isnt available yet');
+});
